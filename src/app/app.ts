3d15927bacc5f180366e247459ad5d587d241141
Franconicovicente <franconicolassvicente@gmail.com>
2026-05-19 08:29:51 -0300

    ultimas modificaciones

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-// 1. Importamos los dos módulos necesarios para las rutas
 import { RouterOutlet, RouterLink } from '@angular/router'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  // 2. Clave: agregamos RouterLink acá para que el HTML entienda el atributo routerLink
   imports: [RouterOutlet, RouterLink], 
-  templateUrl: './app.html', // O './app.component.html' según cómo lo tengas nombrado
-  styleUrls: ['./app.css']   // O './app.component.css'
+  templateUrl: './app.html', 
+  styleUrls: ['./app.css']   
 })
 export class AppComponent {
   title = 'tu-proyecto';
